61cb51b24acf9b3a372dfd9075f824c946a436f1
Rots87 <ncanas@openmailbox.org>
2016-01-15 09:38:21 -0600

    Commit inicial de proyecto

---
+++ src/calculadorarenta/CalculadoraRenta.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package calculadorarenta;
+import calculadorarenta.Sueldo;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author SOPORTE-T
+ */
+public class CalculadoraRenta {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        BufferedReader dato=new BufferedReader (new InputStreamReader(System.in)); 
+        Sueldo S = new Sueldo();
+        S.Sueldo=-1;
+        do{
+            System.out.println("Por favor ingrese el Sueldo \nrecuerde que solo deben de ser numeros\n");
+        try {
+            String d = dato.readLine();
+            S.Sueldo = Float.parseFloat(d);
+        } catch (IOException ex) {
+            System.out.println("Error en la lectura del sueldo\n"+ex);
+        }
+        }while(S.Sueldo<=-1);
+        System.out.println(S.Sueldo);
+        
+    }
+    
+}
---
+++ src/calculadorarenta/Sueldo.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package calculadorarenta;
+
+/**
+ *
+ * @author SOPORTE-T
+ */
+public class Sueldo {
+    float Sueldo;
+    float Seguro;
+    float Afp;
+    float Renta;
+}
